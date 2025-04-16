ef9f47695cba283ea20970c031aeb8b5ae3aa064
Patrick Dagouaga <patrickdagouaga@gmail.com>
2025-04-16 11:50:29 +0000

    Ajout d'un nouve packgage di

---
+++ packages/di/lib/src/env/mobile_services_env.dart
@@ -0,0 +1,4 @@
+import 'package:injectable/injectable.dart';
+
+const hmsDIEnv = Environment('HMS');
+const gmsDIEnv = Environment('GMS');
---
+++ packages/di/lib/src/service_locator.dart
@@ -0,0 +1,4 @@
+import 'package:get_it/get_it.dart';
+
+/// Service locator
+final sl = GetIt.instance;
